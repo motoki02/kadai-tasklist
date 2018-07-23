c7a17b8db0a0b63daed0593b5ca7973820980779
jiro.kirameki <testAAAAA@techmail.com>
2018-07-23 10:01:12 +0000

    bootstrap design

--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -3,17 +3,22 @@
 @section('content')
 
     <h1>id: {{ $task->id }} のタスク編集ページ</h1>
+        <div class="row">
+            <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">
+                {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
+                    <div class="form-group">
+                        {!! Form::label('status', 'ステータス:') !!}
+                        {!! Form::text('status', null, ['class' => 'form-control']) !!}
+                    </div>
 
-    {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
+                    <div class="form-group">
+                        {!! Form::label('content', 'タスク:') !!}
+                        {!! Form::text('content', null, ['class' => 'form-control']) !!}
+                    </div>
 
-        {!! Form::label('status', 'ステータス:') !!}
-        {!! Form::text('status') !!}
-        
-        {!! Form::label('content', 'タスク:') !!}
-        {!! Form::text('content') !!}
+                    {!! Form::submit('更新', ['class' => 'btn btn-default']) !!}
 
-        {!! Form::submit('更新') !!}
-
-    {!! Form::close() !!}
-
+                {!! Form::close() !!}
+            <
+        </div>
 @endsection
